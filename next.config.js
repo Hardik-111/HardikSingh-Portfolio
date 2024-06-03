8872a59d9b4c886e6ef93d9f6d5649f1d2540838
Hardik <ihardik.112@gmail.com>
2024-06-03 18:18:18 +0530

    modified code to add repo secrets to workflow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
-  };
-  
-  module.exports = nextConfig;
-  
+  output: "export",
+  env: {
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+  },
+  images: {
+    unoptimized: true, // Necessary for static exports when using next/image
+  },
+};
+
+module.exports = nextConfig;
